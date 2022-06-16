755fcc5855646d643635adc65a8b4c69d6116f6e
Szabolcs Szana <szabolcs.szana@goto.com>
2022-06-16 12:52:00 +0200

    Add controller

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'conference')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request);
+
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(<<<EOF
         <html>
             <body>
+                $greet
                 <img src="/images/under-construction.gif" />
             </body>
         </html>
